bea169a7ef16d263a9879ad7e2d7e4ccbf45dbb8
Izabela88 <izabkru88@gmail.com>
2022-01-12 11:37:49 +0000

    Add text to about section and style it

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="")
     
 
 @app.route("/contact")
